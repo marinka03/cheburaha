5001a885dab24a60cc62726758e5899df232e549
Marina <chernyakinamarina@gmail.com>
2024-04-12 22:52:26 +0300

    add one category for all card

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
-import style from './HomePage.module.css'
-import Header from '../../components/Header/Header'
+import React from "react";
+import style from "./HomePage.module.css";
+import Header from "../../components/Header/Header";
 
 function HomePage() {
   return (
     <>
-    <Header/>
-    <main className={style.main}>
+      <Header />
+      <main className={style.main}>
         <ul className={style["card-list"]}>
           <li className={style.card}>
-            <h4 className={style['title-card']}>H20</h4>
+            <h4 className={style["title-card"]}>H20</h4>
+            <div className={style.category}>
+              <p>Temperature</p>
+            </div>
           </li>
           <li className={style.card}>
-            <h4 className={style['title-card']}>02</h4>
+            <h4 className={style["title-card"]}>02</h4>
+            <div className={style.category}>
+              <p>Ventilator: </p>
+              <button className={style.btn}>Off</button>
+            </div>
           </li>
           <li className={style.card}>
-            <h4 className={style['title-card']}>Light</h4>
+            <h4 className={style["title-card"]}>Light</h4>
+            <div className={style.category}>
+              <p>Daylight: </p>
+              <button className={style.btn}>Off</button>
+            </div>
+            <div className={style.category}>
+              <p>Night light: </p>
+              <button className={style.btn}>Off</button>
+            </div>
           </li>
         </ul>
       </main>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
