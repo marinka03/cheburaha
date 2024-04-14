ae954e6259e0c588b8c4448c94f2ba3e91f4ae10
Marina <chernyakinamarina@gmail.com>
2024-04-14 14:25:29 +0300

    add two categorys water heating and filter for water, also add styles for categorys-list and btn

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,27 +10,55 @@ function HomePage() {
         <ul className={style["card-list"]}>
           <li className={style.card}>
             <h4 className={style["title-card"]}>H20</h4>
-            <div className={style.category}>
-              <p>Temperature</p>
-            </div>
+            <ul className={style["categorys-list"]}>
+              <li>
+                <div className={style.category}>
+                  <p>Temperature: </p>
+                </div>
+              </li>
+              <li>
+                <div className={style.category}>
+                  <p>Water heating: </p>
+                  <button className={style.btn}>Off</button>
+                </div>
+              </li>
+              <li>
+                <div className={style.category}>
+                  <p>Filter: </p>
+                  <button className={style.btn}>Off</button>
+                </div>
+              </li>
+            </ul>
           </li>
+
           <li className={style.card}>
             <h4 className={style["title-card"]}>02</h4>
-            <div className={style.category}>
-              <p>Ventilator: </p>
-              <button className={style.btn}>Off</button>
-            </div>
+            <ul className={style["categorys-list"]}>
+              <li>
+                <div className={style.category}>
+                  <p>Ventilator: </p>
+                  <button className={style.btn}>Off</button>
+                </div>
+              </li>
+            </ul>
           </li>
+
           <li className={style.card}>
             <h4 className={style["title-card"]}>Light</h4>
-            <div className={style.category}>
-              <p>Daylight: </p>
-              <button className={style.btn}>Off</button>
-            </div>
-            <div className={style.category}>
-              <p>Night light: </p>
-              <button className={style.btn}>Off</button>
-            </div>
+            <ul className={style["categorys-list"]}>
+              <li>
+                <div className={style.category}>
+                  <p>Daylight: </p>
+                  <button className={style.btn}>Off</button>
+                </div>
+              </li>
+              <li>
+                <div className={style.category}>
+                  <p>Night light: </p>
+                  <button className={style.btn}>Off</button>
+                </div>
+              </li>
+            </ul>
           </li>
         </ul>
       </main>
